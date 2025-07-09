e12e775f65dce11677787091ddb1e7f1863f2896
Ashot1114 <ashot.sargsyan16@tumo.org>
2025-07-09 21:23:08 +0400

    Added Card

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <p class="title">My Title</p>
+        <p class="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatibus dolorem sit maiores vero
+            veritatis vitae quo, quia repellat ea earum repellendus at, nobis corrupti rerum sapiente corporis, illo
+            dolor.
+        </p>
+        <p class="time">05-07-2025 20:30</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.card {
+    width: 300px;
+    height: 210px;
+    border: 1px solid #fff;
+    border-radius: 14px;
+    background: linear-gradient(114.43deg, rgba(255, 255, 255, 0.6) 4%, rgba(255, 255, 255, 0.1) 100%);
+}
+</style>
